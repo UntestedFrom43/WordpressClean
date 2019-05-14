7382f5384c6ef659d897e677de06dcff1b135c6f
Ketzenkrad <ketzenkrad@gmail.com>
2019-05-14 19:12:32 +0300

    Loop-цикл

--- wp-content/themes/testtheme2019/index.php
+++ wp-content/themes/testtheme2019/index.php
@@ -1,5 +1,14 @@
 <?php get_header(); ?>
+    <h2>Test</h2>
 
-<h1>Hello, world!</h1>
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+        <!-- Посты -->
+        <h4><?php the_title(); ?></h4>
+        <?php endwhile; ?>
+        <!-- Навигация-->
+        <?php else: ?>
+        <!-- Постов нет -->
+        <p>No post in there...</p>
+    <?php endif; ?>
 
-<?php get_footer(); ?>
+<?php get_footer();?>
