42066c72126567d2744260a13e1369ba40e9fbc6
Ketzenkrad <ketzenkrad@gmail.com>
2019-05-14 22:53:27 +0300

    Создан single-шаблон для статей

--- wp-content/themes/testtheme2019/index.php
+++ wp-content/themes/testtheme2019/index.php
@@ -1,6 +1,6 @@
-<?php get_header(); ?>
+    <!-- Стартовый шаблон -->
+    <?php get_header(); ?>
     <h2>Test</h2>
-
     <div class="container">
         <div class="row">
             <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
@@ -8,17 +8,16 @@
         <div class="col-md-12">
             <div class="card">
             <img src="..." class="card-img-top" alt="Место для миниатюры">
-                <div class="card-body">
-                
+            <!-- Карточки постов -->
+                <div class="card-body">            
                 <h5 class="card-title">
                     <a href="<?php the_permalink(); ?>">
                         <?php the_title(); ?>
                     </a>
                 </h5>
-
-                <p class="card-text"><?php the_content('Читать статью далее...'); ?></p>
+                <!-- Линки -->
+                <p class="card-text"><?php the_excerpt(); ?></p>
                 <a href="<?php the_permalink(); ?>" class="btn btn-primary">Перейти в статью</a>
-                
                 </div>
             </div>
         </div>
@@ -30,5 +29,4 @@
             <?php endif; ?>    
         </div>
     </div>
-
-<?php get_footer();?>
+    <?php get_footer();?>
---
+++ wp-content/themes/testtheme2019/single.php
@@ -0,0 +1,24 @@
+<!-- Single-шаблон -->
+<?php get_header(); ?>
+    <h2>Test</h2>
+    <div class="container">
+        <div class="row">
+            <?php while ( have_posts() ) : the_post(); ?>
+        <!-- Посты -->
+        <div class="col-md-12">
+            <div class="card">
+            <img src="..." class="card-img-top" alt="Место для миниатюры">
+            <!-- Карточки постов -->
+                <div class="card-body">            
+                <h2 class="card-title">
+                    <?php the_title(); ?>
+                </h2>
+                <!-- Линки -->
+                <p class="card-text"><?php the_content(''); ?></p>
+                </div>
+            </div>
+        </div>
+            <?php endwhile; ?>
+        </div>
+    </div>
+<?php get_footer();?>
