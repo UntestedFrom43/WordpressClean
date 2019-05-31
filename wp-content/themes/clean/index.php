eab855810bcc904a615e2d92ba0c05a09e696aee
Ketzenkrad <ketzenkrad@gmail.com>
2019-05-31 18:43:00 +0300

    Мультиязычность и фреймворк

--- wp-content/themes/clean/index.php
+++ wp-content/themes/clean/index.php
@@ -22,12 +22,15 @@
 			<!-- В случае отсутствия миниатюры/изображения к посту/ -->
 			<?php if( has_post_thumbnail($post) ){ $img_url = get_the_post_thumbnail_url($post, $size); }else{ $img_url = 'https://picsum.photos/1280/864?grayscale'; }?>
 			<!-- Посты -->
-			<!-- Реализация счетчика на каждый второй пост, dynamic classes -->
+			<!-- Реализация счетчика на каждый второй пост, меняем класс динамически -->
 			<div class="fh5co-portfolio-item <?php if( $i % 2 == 0 ) echo 'fh5co-img-right'; ?>">
 				<div class="fh5co-portfolio-figure animate-box" style="background-image: url(<?php echo $img_url; ?>);"></div>
 				<div class="fh5co-portfolio-description">
+					<!-- Передадим заголовок -->
 					<h2> <?php the_title($before, $after, $echo); ?> </h2>
+					<!-- Содержимое в виде параграфа -->
 					<p> <?php the_content('', $strip_teaser); ?> </p>
+					<!-- И ссылочку -->
 					<p><a href="<?php the_permalink($post);?>" class="btn btn-primary"> <?php _e('Read more', 'clean');?> </a></p>
 				</div>
 			</div>
